39ed97effe488e0775d967a283862a35ad360a4d
MaidSafe-QA <qa@maidsafe.net>
2016-06-16 15:01:24 +0000

    Updated documentation.

---
+++ master/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['libsodium_seeded_prng'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='libsodium_seeded_prng/enum.Error.html' title='libsodium_seeded_prng::Error'>Error</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='libsodium_seeded_prng/struct.Seed.html' title='libsodium_seeded_prng::Seed'>Seed</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
